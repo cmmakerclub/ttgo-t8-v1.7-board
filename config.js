cfe3c4cc2a670cd9c7756fa38a974ba9419a54ea
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-07-11 19:05:20 +0700

    update version board

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   email: "makerasia@gmail.com",
   git: "https://github.com/cmmakerclub/ttgo-t8-v1.7-board/",
   image: "/static/display.jpg",
-  version: "1.0.0",
+  version: "1.0.1",
 };
