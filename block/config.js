4bfc819edea8b071c9c58bee3e090fcba72314ce
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-07-21 01:29:43 +0700

    update block

--- block/config.js
+++ block/config.js
@@ -113,6 +113,49 @@ module.exports = {
                 'esp32_servo_read',
                 'esp32_servo_read_micros'
             ]
+        },
+        {
+        name: "SD Card",
+        color: "65",
+        icon: "/static/icons/icons8_stack_96px.png",
+        blocks: [
+           "sdcard_begin",
+           {
+            xml:
+              `<block type="sdcard_write_txt">
+                  <value name="DATA">
+                      <shadow type="basic_string">
+                          <field name="VALUE">Hello KB-IDE</field>
+                      </shadow>
+                  </value>
+              </block>`
+           },
+           {
+            xml:
+              `<block type="sdcard_write_csv">
+                  <value name="DATA1">
+                      <shadow type="basic_string">
+                          <field name="VALUE"></field>
+                      </shadow>
+                  </value>
+                  <value name="DATA2">
+                      <shadow type="basic_string">
+                          <field name="VALUE"></field>
+                      </shadow>
+                  </value>
+                  <value name="DATA3">
+                      <shadow type="basic_string">
+                          <field name="VALUE"></field>
+                      </shadow>
+                  </value>
+                  <value name="DATA4">
+                      <shadow type="basic_string">
+                          <field name="VALUE"></field>
+                      </shadow>
+                  </value>
+              </block>`
+           }
+        ]
         },        
         {
             name : 'Time',
@@ -139,15 +182,6 @@ module.exports = {
                         </value>
                     </block>`
                 },
-                'time_wait_btn_press',
-                'time_sync',
-                'time_get_year',
-                'time_get_month',
-                'time_get_day',
-                'time_get_day_of_week',
-                'time_get_hour',
-                'time_get_minute',
-                'time_get_second',
                 'time_millis',
                 'time_micros'
             ]
